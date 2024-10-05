a78ec72d1b93cd712dc87979edae2c601dc1a6e9
rhrits <hritik.hritiksingh@gmail.com>
2024-10-06 04:49:00 +0530

    change

--- fn4.js
+++ fn4.js
@@ -2,5 +2,6 @@
 // Write a function that takes a new object as input which has name , age  and gender and greets the user with their gender (Hi Mr/Mrs/Others Hriitk, your age is 21)
 
 function greetwithgender(user){
-    let
+    let greeting ="";
+    if (user.gender)
 }
